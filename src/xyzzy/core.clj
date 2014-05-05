a01c939ee71c24a1d314b329f8dc6ea14d5471c2
Max Kreminski <maxkreminski@gmail.com>
2014-05-05 09:28:35 -0700

    Simple path movement and helper functions

--- src/xyzzy/core.clj
+++ src/xyzzy/core.clj
@@ -1,6 +1,44 @@
 (ns xyzzy.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; path movement
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defn- sibling [path n]
+  (when (>= n 0) (conj (pop path) n)))
+
+(defn- down* [path]
+  (conj path 0))
+
+(defn- left* [path]
+  (when (seq path) (sibling path (dec (peek path)))))
+
+(defn leftmost* [path]
+  (when (seq path) (sibling path 0)))
+
+(defn- right* [path]
+  (when (seq path) (sibling path (inc (peek path)))))
+
+(defn- up* [path]
+  (when (seq path) (pop path)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; path and zipper helpers
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defn- full-path [path]
+  (vec (interleave (repeat :children) path)))
+
+(defn node [{:keys [tree path]}]
+  (get-in tree (full-path path)))
+
+(defn branch?
+  "Tests whether `node` is a branch node (i.e. permitted to have children)."
+  [node]
+  (contains? node :children))
+
+(defn- check
+  "Tests whether `(:path loc)` points to an extant node in `(:tree loc)`,
+   returning `loc` if the test passes and `nil` if it does not."
+  [loc]
+  (when (and (:path loc) (node loc)) loc))
