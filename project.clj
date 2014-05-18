b7335ce06db4df6453499876e7aea7d6ede3bfd1
Max Kreminski <maxkreminski@gmail.com>
2014-05-18 09:28:20 -0400

    Use cljx to build for both Clojure and ClojureScript

--- project.clj
+++ project.clj
@@ -1,4 +1,11 @@
 (defproject xyzzy "0.1.0-SNAPSHOT"
   :description "Smarter zippers for Clojure"
   :url "http://github.com/mkremins/xyzzy"
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :plugins [[com.keminglabs/cljx "0.3.2"]]
+  :cljx {:builds [{:source-paths ["src"]
+                   :output-path "target/classes"
+                   :rules :clj}
+                  {:source-paths ["src"]
+                   :output-path "target/classes"
+                   :rules :cljs}]})
