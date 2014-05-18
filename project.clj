6f96c21dc6a0f058a8f00091fbc4a591edbc1a70
Max Kreminski <maxkreminski@gmail.com>
2014-05-18 09:38:33 -0400

    Bump version to 0.1.1-SNAPSHOT and add license

--- project.clj
+++ project.clj
@@ -1,6 +1,9 @@
-(defproject mkremins/xyzzy "0.1.0"
+(defproject mkremins/xyzzy "0.1.1-SNAPSHOT"
   :description "Smarter zippers for Clojure"
   :url "http://github.com/mkremins/xyzzy"
+  :license {:name "MIT License"
+            :url "http://opensource.org/licenses/MIT"
+            :distribution :repo}
   :dependencies [[org.clojure/clojure "1.6.0"]]
   :plugins [[com.keminglabs/cljx "0.3.2"]]
   :cljx {:builds [{:source-paths ["src"]
