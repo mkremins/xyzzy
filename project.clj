77462987f3e3dae4d6189547f440d0c051ef3f75
Max Kreminski <maxkreminski@gmail.com>
2014-05-18 09:33:28 -0400

    Rename xyzzy => mkremins/xyzzy for deployment to Clojars

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject xyzzy "0.1.0-SNAPSHOT"
+(defproject mkremins/xyzzy "0.1.0-SNAPSHOT"
   :description "Smarter zippers for Clojure"
   :url "http://github.com/mkremins/xyzzy"
   :dependencies [[org.clojure/clojure "1.6.0"]]
