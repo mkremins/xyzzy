5be2b68f267fbab77d820a6b6c113e3df9f79bb3
Max Kreminski <maxkreminski@gmail.com>
2014-05-21 07:04:11 -0400

    Release v0.1.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject mkremins/xyzzy "0.1.2-SNAPSHOT"
+(defproject mkremins/xyzzy "0.1.2"
   :description "Smarter zippers for Clojure"
   :url "http://github.com/mkremins/xyzzy"
   :license {:name "MIT License"
