8b5032f2bbd39955ce3b1e791ca82897934d7ff1
Max Kreminski <maxkreminski@gmail.com>
2014-09-01 09:23:18 -0700

    Temporarily revert from cljx 0.4.0 => 0.3.2

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "http://opensource.org/licenses/MIT"
             :distribution :repo}
   :dependencies [[org.clojure/clojure "1.6.0"]]
-  :plugins [[com.keminglabs/cljx "0.4.0"]]
+  :plugins [[com.keminglabs/cljx "0.3.2"]]
   :cljx {:builds [{:source-paths ["src"]
                    :output-path "target/classes"
                    :rules :clj}
