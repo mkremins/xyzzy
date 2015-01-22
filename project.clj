e3c0a94a7cca5c718715e90ad1c15f1d7c37ca0c
Max Kreminski <maxkreminski@gmail.com>
2015-01-22 08:18:30 -0800

    Bump version to 0.3.3-SNAPSHOT for bugfix

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject mkremins/xyzzy "0.3.2"
+(defproject mkremins/xyzzy "0.3.3-SNAPSHOT"
   :description "Smarter zippers for Clojure"
   :url "http://github.com/mkremins/xyzzy"
   :license {:name "MIT License"
